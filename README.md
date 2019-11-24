044d8b4343ce05d71333c727ed6e3313b1dc453f
Jonathan SAHM <contact@johnstyle.fr>
2019-11-24 18:11:50 +0100

    Create bundle

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rhapsody Bundle
+
+This bundle allows the creation of simple and fast administration for Symfony 4 and 5.
---
+++ src/RhapsodyBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PhpGuild\RhapsodyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class RhapsodyBundle
+ */
+class RhapsodyBundle extends Bundle
+{
+}
